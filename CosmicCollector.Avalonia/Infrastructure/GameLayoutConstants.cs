3bf471598584c8fff49d6cbd4004a696f2bf8e48
vodoem <prosto_duz@mail.ru>
2026-01-14 19:31:01 +0300

    ласт версия

--- CosmicCollector.Avalonia/Infrastructure/GameLayoutConstants.cs
+++ CosmicCollector.Avalonia/Infrastructure/GameLayoutConstants.cs
@@ -8,12 +8,12 @@ internal static class GameLayoutConstants
   /// <summary>
   /// Ширина игрового мира.
   /// </summary>
-  public const double WorldWidth = 800;
+  public const double WorldWidth = 750;
 
   /// <summary>
   /// Высота игрового мира.
   /// </summary>
-  public const double WorldHeight = 600;
+  public const double WorldHeight = 750;
 
   /// <summary>
   /// Высота HUD в пикселях.
